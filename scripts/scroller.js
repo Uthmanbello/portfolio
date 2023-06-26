1a0e524dc9217239b261c47a0309c4ed8ce9e97c
Uthmanbello <uthmanbello88@gmail.com>
2023-06-26 14:25:27 +0100

    Setup auto scrollable project images for popup

--- scripts/scroller.js
+++ scripts/scroller.js
@@ -1,45 +1,89 @@
-const container = document.querySelector('.popup-images-container');
-const images = document.querySelector('.popup-images');
-let isScrolling = false;
-let startX;
-let scrollLeft;
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('.popup-images-container');
+  const images = document.querySelector('.popup-images');
+  const dots = document.querySelectorAll('.popup-dot');
 
-container.addEventListener('mousedown', (e) => {
-  isScrolling = true;
-  startX = e.pageX - container.offsetLeft;
-  scrollLeft = container.scrollLeft;
-});
+  let isScrolling = false;
+  let startX;
+  let scrollLeft;
+  let currentIndex = 0;
+  let interval;
 
-container.addEventListener('mouseleave', () => {
-  isScrolling = false;
-});
+  function setActiveDot(index) {
+    dots.forEach((dot, i) => {
+      dot.classList.toggle('active', i === index);
+    });
+  }
 
-container.addEventListener('mouseup', () => {
-  isScrolling = false;
-});
+  function scrollToImage(index) {
+    const imageWidth = container.offsetWidth;
+    container.scrollLeft = imageWidth * index;
+    currentIndex = index;
+    setActiveDot(currentIndex);
+  }
 
-container.addEventListener('mousemove', (e) => {
-  if (!isScrolling) return;
-  e.preventDefault();
-  const x = e.pageX - container.offsetLeft;
-  const walk = x - startX;
-  container.scrollLeft = scrollLeft - walk;
-});
+  function startSwipeInterval() {
+    interval = setInterval(() => {
+      if (currentIndex < images.children.length - 1) {
+        scrollToImage(currentIndex + 1);
+      } else {
+        scrollToImage(0);
+      }
+    }, 5000);
+  }
 
-container.addEventListener('touchstart', (e) => {
-  isScrolling = true;
-  startX = e.touches[0].pageX - container.offsetLeft;
-  scrollLeft = container.scrollLeft;
-});
+  container.addEventListener('mousedown', (e) => {
+    clearInterval(interval);
+    isScrolling = true;
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
 
-container.addEventListener('touchend', () => {
-  isScrolling = false;
-});
+  container.addEventListener('mouseleave', () => {
+    isScrolling = false;
+    startSwipeInterval();
+  });
 
-container.addEventListener('touchmove', (e) => {
-  if (!isScrolling) return;
-  e.preventDefault();
-  const x = e.touches[0].pageX - container.offsetLeft;
-  const walk = x - startX;
-  container.scrollLeft = scrollLeft - walk;
+  container.addEventListener('mouseup', () => {
+    isScrolling = false;
+    startSwipeInterval();
+  });
+
+  container.addEventListener('mousemove', (e) => {
+    if (!isScrolling) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = x - startX;
+    container.scrollLeft = scrollLeft - walk;
+  });
+
+  container.addEventListener('touchstart', (e) => {
+    clearInterval(interval);
+    isScrolling = true;
+    startX = e.touches[0].pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
+
+  container.addEventListener('touchend', () => {
+    isScrolling = false;
+    startSwipeInterval();
+  });
+
+  container.addEventListener('touchmove', (e) => {
+    if (!isScrolling) return;
+    e.preventDefault();
+    const x = e.touches[0].pageX - container.offsetLeft;
+    const walk = x - startX;
+    container.scrollLeft = scrollLeft - walk;
+  });
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+      scrollToImage(index);
+      clearInterval(interval);
+      startSwipeInterval();
+    });
+  });
+
+  startSwipeInterval();
 });
