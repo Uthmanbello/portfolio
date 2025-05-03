10f79a1a25ee0278ae39487609c521253fd4bd07
Uthmanbello <uthmanbello88@gmail.com>
2025-05-03 13:24:22 +0100

    Add pdf

--- scripts/projects.js
+++ scripts/projects.js
@@ -7,7 +7,8 @@ const projectArr = [
       description: 'An e-commerce mobile app that allows customers to buy various products and have them delivered with ease. (For the best layout and user experience, view on a mobile device.)',
       technologies: ['javascript', 'react', 'firebase'],
       live: 'https://kasuwararewa.vercel.app/',
-      source: '#'
+      source: '#',
+      mobile: '(mobile)'
   },
   {
       id: 1,
@@ -17,7 +18,8 @@ const projectArr = [
       description: "A mobile app that allows users to subscribe to internet data bundles, pay for electricity and cable TV, and even purchase call recharge cards. (For the best layout and user experience, view on a mobile device.)",
       technologies: ['javascript', 'react', 'firebase'],
       live: 'https://subexpress.vercel.app/',
-      source: '#'
+      source: '#',
+      mobile: '(mobile)'
   },
   {
     id: 2,
@@ -27,57 +29,74 @@ const projectArr = [
     description: "An automated academic portal where students learn full-stack development and receive career training and support.",
     technologies: ['javascript', 'react', 'firebase'],
     live: 'https://www.elevateherspace.com/',
-    source: '#'
+    source: '#',
+    mobile: ''
   },
   {
-    id: 3,
+  id: 3,
+  title: "DeRoyale Foods",
+  mobileImage: 'images/foods-mob.png',
+  desktopImage: 'images/foods-desk.png',
+  description: "DeRoyale Foods is a project built with SASS and Bootstrap which displays different meals and their recipes. It also provides links for users to watch videos on how to prepare their favorite dishes.",
+  technologies: ['html', 'css', 'javascript'],
+  live: 'https://uthmanbello.github.io/deroyale-foods/',
+  source: 'https://github.com/Uthmanbello/deroyale-foods',
+  mobile: ''
+},
+  {
+    id: 4,
     title: "D Power Doctor's Channel",
     mobileImage: 'images/powerdoctor-mob.PNG',
     desktopImage: 'images/powerdoctor-desk.PNG',
     description: "An online channel where viewers can watch and read episodes and articles from D Power Doctor. Users can also purchase books whenever they become available.",
     technologies: ['javascript', 'react', 'firebase'],
     live: 'https://dpowerdoctor.vercel.app/',
-    source: '#'
+    source: '#',
+    mobile: ''
   },
   {
-    id: 4,
+    id: 5,
     title: 'Vitafoam Store',
     mobileImage: 'images/vitafoam-mob.PNG',
     desktopImage: 'images/vitafoam-desk.PNG',
     description: "An e-commerce mobile app that allows customers to buy various products from Vitafoam. (For the best layout and user experience, view on a mobile device.)",
     technologies: ['javascript', 'react', 'firebase'],
     live: 'https://vitafoam-app.vercel.app/',
-    source: '#'
+    source: '#',
+    mobile: '(mobile)'
   },
   {
-      id: 5,
+      id: 6,
       title: 'Bookstore',
       mobileImage: 'images/bookstore-mob.png',
       desktopImage: 'images/bookstore-desk.png',
       description: 'Bookstore is a project that uses a Single-Page Application format to display items on a page. A user can switch to two pages wherein one displays the titles and authors of books, and also a form to add a new book.',
       technologies: ['css', 'javascript', 'react/redux'],
       live: 'https://bookstore-gold-alpha.vercel.app/',
-      source: 'https://github.com/Uthmanbello/bookstore'
+      source: 'https://github.com/Uthmanbello/bookstore',
+      mobile: ''
   },
   {
-      id: 6,
+      id: 7,
       title: 'Weather App',
       mobileImage: 'images/weather-mob.png',
       desktopImage: 'images/weather-desk.png',
       description: 'Weather App is a project that uses a Single-Page Application format to display items on a page. A user can access different weather information by simply clicking on the relevant category.',
       technologies: ['css', 'javascript', 'react/redux'],
       live: 'https://weather-update-app.vercel.app/',
-      source: 'https://github.com/Uthmanbello/metrics-webapp'
+      source: 'https://github.com/Uthmanbello/metrics-webapp',
+      mobile: ''
   },
   {
-      id: 7,
+      id: 8,
       title: 'Math Magicians',
       mobileImage: 'images/math-mob.png',
       desktopImage: 'images/math-desk.png',
       description: 'Math Magician is a project that uses a Single-Page Application format to display items on a page. A user can switch to a page where a live calculator which can be used for computation is.',
       technologies: ['html', 'css', 'javascript'],
       live: 'https://math-magicians-chi.vercel.app/',
-      source: 'https://github.com/Uthmanbello/math-magicians'
+      source: 'https://github.com/Uthmanbello/math-magicians',
+      mobile: ''
   },
   // {
   //     id: 5,
@@ -90,14 +109,15 @@ const projectArr = [
   //     source: 'https://github.com/Uthmanbello/tutoring-app-frontend'
   // },
   {
-      id: 8,
+      id: 9,
       title: "Space Travelers' Hub",
       mobileImage: 'images/space travellers-mob.png',
       desktopImage: 'images/space travellers-desk.png',
       description: "Space Travellers' Hub is a project which works with live data from the SpaceX API. The app allows users to book rockets and join selected space missions.",
       technologies: ['css', 'javascript', 'react/redux'],
       live: 'https://space-travellers-hub-two.vercel.app/',
-      source: 'https://github.com/Uthmanbello/react_group_project'
+      source: 'https://github.com/Uthmanbello/react_group_project',
+      mobile: ''
   },
 //   {
 //     id: 7,
@@ -120,24 +140,15 @@ const projectArr = [
 //   source: 'https://github.com/Uthmanbello/Recipe-app'
 // },
 {
-  id: 9,
+  id: 10,
   title: "My Review Partner",
   mobileImage: 'images/review-mob1.PNG',
   desktopImage: 'images/review-desk1.PNG',
   description: "My Review Partner is a responsive AI powered project which was built using React. It fetches reviews from popular social media and gives responses to the reviews automatically. The user can also edit the responses as they deem fit. The app also provides the user with the flexibility of navigating through reviews and the generated responses.",
   technologies: ['css', 'javascript', 'react'],
   live: 'https://manager-review-platform.vercel.app/',
-  source: 'https://github.com/Uthmanbello/review-platform-front'
-},
-{
-  id: 10,
-  title: "DeRoyale Foods",
-  mobileImage: 'images/foods-mob.png',
-  desktopImage: 'images/foods-desk.png',
-  description: "DeRoyale Foods is a project built with SASS and Bootstrap which displays different meals and their recipes. It also provides links for users to watch videos on how to prepare their favorite dishes.",
-  technologies: ['html', 'css', 'javascript'],
-  live: 'https://uthmanbello.github.io/deroyale-foods/',
-  source: 'https://github.com/Uthmanbello/deroyale-foods'
+  source: 'https://github.com/Uthmanbello/review-platform-front',
+  mobile: ''
 },
 {
   id: 11,
@@ -147,7 +158,8 @@ const projectArr = [
   description: "A Single-Page Application built with JavaScript which interacts with two APIs. It is an app wherein users can 'like' respective seasons of 'The 100' series, add comments and reservations too.",
   technologies: ['html', 'css', 'javascript'],
   live: 'https://uthmanbello.github.io/capstone2/dist',
-  source: 'https://github.com/Uthmanbello/capstone2'
+  source: 'https://github.com/Uthmanbello/capstone2',
+  mobile: ''
 },
 {
   id: 12,
@@ -157,7 +169,8 @@ const projectArr = [
   description: "An app that uses AI to interview a potential customer for a hotel. It is built using React.",
   technologies: ['react', 'css', 'javascript'],
   live: 'https://deroyale-lodge.vercel.app/',
-  source: 'https://github.com/Uthmanbello/deroyale-companion'
+  source: 'https://github.com/Uthmanbello/deroyale-companion',
+  mobile: ''
 }
 ]
 
@@ -178,7 +191,7 @@ projectArr.forEach((project) => {
           <span>${project.technologies[1]}</span>
           <span>${project.technologies[2]}</span>
         </div>
-        <button type="button" class="center card-button">See Project</button>
+        <a href="${project.live}" class="center card-button" target="_blank">See Demo <span style="font-size: 0.7rem">${project.mobile}</span></a>
       </div>
     </div>
   `;
@@ -186,32 +199,32 @@ projectArr.forEach((project) => {
 
 cards.innerHTML = html;
 
-const projectButtons = document.querySelectorAll('.card-button');
+// const projectButtons = document.querySelectorAll('.card-button');
 
-for (let index = 0; index < projectButtons.length; index++) {
-  projectButtons[index].addEventListener('click', () => {
-      const title = popup.querySelector('.popup-title');
-      const liveButton = popup.querySelector('.see-live');
-      // const sourceButton = popup.querySelector('.see-source');
-      const popupContent = popup.querySelector('.popup-content');
+// for (let index = 0; index < projectButtons.length; index++) {
+//   projectButtons[index].addEventListener('click', () => {
+//       const title = popup.querySelector('.popup-title');
+//       const liveButton = popup.querySelector('.see-live');
+//       // const sourceButton = popup.querySelector('.see-source');
+//       const popupContent = popup.querySelector('.popup-content');
 
-      title.textContent = projectArr[index].title;
-      if (window.innerWidth < 768) {
-        popupContent.style.backgroundImage = `url('${projectArr[index].mobileImage}')`;
-      } else {
-        popupContent.style.backgroundImage = `url('${projectArr[index].desktopImage}')`;
-      }
+//       title.textContent = projectArr[index].title;
+//       if (window.innerWidth < 768) {
+//         popupContent.style.backgroundImage = `url('${projectArr[index].mobileImage}')`;
+//       } else {
+//         popupContent.style.backgroundImage = `url('${projectArr[index].desktopImage}')`;
+//       }
 
-      liveButton.setAttribute('href', projectArr[index].live);
-      // sourceButton.setAttribute('href', projectArr[index].source);
+//       liveButton.setAttribute('href', projectArr[index].live);
+//       // sourceButton.setAttribute('href', projectArr[index].source);
 
-      popup.classList.add('show')
-      document.body.style.overflowY = 'hidden';
-  }) 
-}
+//       popup.classList.add('show')
+//       document.body.style.overflowY = 'hidden';
+//   }) 
+// }
 
-const btnClosePopup = document.querySelector('.popup-close, .popup-header');
-btnClosePopup.addEventListener('click', () => {
-    popup.classList.remove('show');
-    document.body.style.overflowY = 'auto';
-})
+// const btnClosePopup = document.querySelector('.popup-close, .popup-header');
+// btnClosePopup.addEventListener('click', () => {
+//     popup.classList.remove('show');
+//     document.body.style.overflowY = 'auto';
+// })
